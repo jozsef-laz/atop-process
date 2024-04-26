13c5dc10004d02665963eba03a3e319eccdefaa0
Jozsef Laz <jlaz@purestorage.com>
2024-04-26 16:53:10 +0200

    creating result.csv

---
+++ commands.md
@@ -0,0 +1,22 @@
+```
+time ./run-ir-test.sh --clusters=irp871-c76,irp871-c77 -x
+time /ir_test/tools/bld_linux/bin/fsstress --config /ir_test/tools/bld_linux/fill_sim.cfg --duration 90 --server 10.88.210.197 --timeout 180 --path /hedgehog/left,/hedgehog/right --nfsdtype SIMULATION
+purefs snap --send --target irp871-c77 hedgehog
+```
+
+in cmux:
+```
+cmux -na
+rm -f alma.raw; atop -w alma.raw 5
+```
+
+collecting logs:
+```
+time collect.sh -c irp871-c76 -l atop_blades
+```
+
+stopping `atop` from cmux:
+signal 12 is SIGUSR2
+```
+PP=$(ps aux | grep alma.raw | grep -v grep | tr -s ' ' | cut -d' ' -f2); echo $PP; kill -s 12 $PP
+```
--- process.py
+++ process.py
@@ -1,27 +1,55 @@
 import sys
-import datetime
 
 class Sample:
-    def __init__(self, timestamp: str, systick: str, usertick: str):
-        self.dt = datetime.datetime.fromtimestamp(int(timestamp))
+    def __init__(self, systick: str, usertick: str):
         self.systick = int(systick)
         self.usertick = int(usertick)
+    def __str__(self):
+        return f"<sys={self.systick}, user={self.usertick}>"
 
-samples: list[Sample] = []
-firstSkipped = False
+# dict: timestamp -> list of samples (from each blade exactly 1)
+samples: dict[int, list[Sample]] = {}
+# the first batch of data of each log is some non-sense data,
+# so let's just skip it, and iterate until next SEP line
+continueTillNextSEP = False
 for line in sys.stdin:
-    if line.startswith('SEP') or line.startswith('RESET'):
+    if line.startswith('SEP'):
+        if continueTillNextSEP:
+            continueTillNextSEP = False
         continue
-    if not firstSkipped:
-        # skip the first
-        firstSkipped = True
+    if line.startswith('RESET'):
+        # next blade's log is coming
+        continueTillNextSEP = True
+        continue
+    if continueTillNextSEP:
         continue
     if line.startswith('CPU'):
         # example:
         #CPU ch1-fb1 1714061219 2024/04/25 18:06:59 5 100 10 434 731 476 3142 4 0 85 1 0 21940 100 0 0
         one_line_data = line.split()
-        one_sample = Sample(one_line_data[2], one_line_data[8], one_line_data[9])
-        samples.append(one_sample)
+        one_sample = Sample(one_line_data[8], one_line_data[9])
+        timestamp = int(one_line_data[2])
+        if timestamp in samples:
+            samples[timestamp].append(one_sample)
+        else:
+            samples[timestamp] = [one_sample]
 
-for s in samples:
-    print(f'{s.dt}, {s.systick}, {s.usertick}')
+# creating average numbers
+startTimestamp = min(samples.keys())
+averages: dict[int, Sample] = {}
+numOfBlades=len(next(iter(samples.values())))
+for timestamp, sampleList in samples.items():
+    #print(f'{timestamp}, list: {", ".join([str(sample) for sample in sampleList])}')
+    assert len(sampleList) == numOfBlades, f"at timestamp {timestamp} we have different number of samples"
+    systicks = [sample.systick for sample in sampleList]
+    userticks = [sample.usertick for sample in sampleList]
+    systick_average = round(float(sum(systicks)) / numOfBlades)
+    usertick_average = round(float(sum(userticks)) / numOfBlades)
+    relativeTimestamp = timestamp-startTimestamp
+    averages[relativeTimestamp] = Sample(systick_average, usertick_average)
+
+# write out average numbers to a csv
+with open("result.csv", "w") as f:
+    f.write(f'# timestamp, systick, usertick\n')
+    for timestamp, sample in averages.items():
+        f.write(f'{timestamp},{sample.systick},{sample.usertick}\n')
