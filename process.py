46da40943114ed624d14003ac33a153aa3c93bb5
Jozsef Laz <jlaz@purestorage.com>
2024-04-30 15:29:22 +0200

    running multiple tests, and processing also adjusted

--- process.py
+++ process.py
@@ -1,55 +1,86 @@
 import sys
+import math
 
 class Sample:
     def __init__(self, systick: str, usertick: str):
         self.systick = int(systick)
         self.usertick = int(usertick)
+        self.systick_std_dev = 0
+        self.usertick_std_dev = 0
+
+    def set_std_dev(self, systick_std_dev, usertick_std_dev):
+        self.systick_std_dev = systick_std_dev
+        self.usertick_std_dev = usertick_std_dev
+
     def __str__(self):
         return f"<sys={self.systick}, user={self.usertick}>"
 
-# dict: timestamp -> list of samples (from each blade exactly 1)
+def average(numbers: list[int]) -> float:
+    return float(sum(numbers)) / len(numbers)
+
+def std_dev(numbers: list[int]) -> float:
+    sqrs = [n*n for n in numbers]
+    a = average(numbers)
+    sa = average(sqrs)
+    print(f'a={a}, sa={sa}')
+    return math.sqrt(sa - a*a)
+
+# dict: time -> list of samples
+#   - where time is the elapsed time from the start of each test
+#   - each sample is an instance of `Sample`, containing values of a blade
+# so with multiple test runs, each blade provides multiple samples for each time=0, 5, ...
 samples: dict[int, list[Sample]] = {}
 # the first batch of data of each log is some non-sense data,
 # so let's just skip it, and iterate until next SEP line
 continueTillNextSEP = False
+startTimestamp = 0
 for line in sys.stdin:
     if line.startswith('SEP'):
         if continueTillNextSEP:
             continueTillNextSEP = False
         continue
     if line.startswith('RESET'):
+        # after each RESET, new data series starts
+        startTimestamp = 0
         # next blade's log is coming
         continueTillNextSEP = True
         continue
     if continueTillNextSEP:
         continue
+
+    # processing real data
+    if startTimestamp == 0:
+        one_line_data = line.split()
+        startTimestamp = int(one_line_data[2])
     if line.startswith('CPU'):
         # example:
         #CPU ch1-fb1 1714061219 2024/04/25 18:06:59 5 100 10 434 731 476 3142 4 0 85 1 0 21940 100 0 0
         one_line_data = line.split()
         one_sample = Sample(one_line_data[8], one_line_data[9])
-        timestamp = int(one_line_data[2])
-        if timestamp in samples:
-            samples[timestamp].append(one_sample)
+        relativeTime = int(one_line_data[2]) - startTimestamp
+        if relativeTime in samples:
+            samples[relativeTime].append(one_sample)
         else:
-            samples[timestamp] = [one_sample]
+            samples[relativeTime] = [one_sample]
 
 # creating average numbers
 startTimestamp = min(samples.keys())
 averages: dict[int, Sample] = {}
 numOfBlades=len(next(iter(samples.values())))
 for timestamp, sampleList in samples.items():
-    #print(f'{timestamp}, list: {", ".join([str(sample) for sample in sampleList])}')
-    assert len(sampleList) == numOfBlades, f"at timestamp {timestamp} we have different number of samples"
+    #l = ", ".join([str(s) for s in sampleList])
+    #print(f"at timestamp {timestamp} we have [{len(sampleList)}] number of samples: {l}")
     systicks = [sample.systick for sample in sampleList]
     userticks = [sample.usertick for sample in sampleList]
-    systick_average = round(float(sum(systicks)) / numOfBlades)
-    usertick_average = round(float(sum(userticks)) / numOfBlades)
+    sample = Sample(average(systicks), average(userticks))
+
     relativeTimestamp = timestamp-startTimestamp
-    averages[relativeTimestamp] = Sample(systick_average, usertick_average)
+    sample.set_std_dev(std_dev(systicks), std_dev(userticks))
+
+    averages[relativeTimestamp] = sample
 
 # write out average numbers to a csv
 with open("result.csv", "w") as f:
-    f.write(f'# timestamp, systick, usertick\n')
+    f.write(f'# timestamp, systick, systick.std_dev, usertick, usertick.std_dev\n')
     for timestamp, sample in averages.items():
-        f.write(f'{timestamp},{sample.systick},{sample.usertick}\n')
+        f.write(f'{timestamp},{sample.systick},{sample.systick_std_dev},{sample.usertick},{sample.usertick_std_dev}\n')
--- run-test.sh
+++ run-test.sh
@@ -17,32 +17,55 @@ if [ -z "$DATAVIP" ]; then
     echo "Error: there's no data vip on the cluster"
     exit 1
 fi
-time /home/ir/work/first/run-ir-test.sh --clusters=irp871-c76,irp871-c77 -x
-retval_check $?
-time sshpass -p welcome ssh $SSHARGS ir@irp871-c76h01 "/ir_test/tools/bld_linux/bin/fsstress --config /ir_test/tools/bld_linux/fill_sim.cfg --duration 90 --server 10.88.210.197 --timeout 180 --path /hedgehog/left,/hedgehog/right --nfsdtype SIMULATION"
-retval_check $?
+NUM_OF_TESTRUNS=3
+date
+echo "---> deleting alma.raw files on cluster <---"
 sshpass -p welcome ssh $SSHARGS ir@irp871-c76 "exec.py -na \"rm -rf /home/ir/alma.raw\""
-sshpass -p welcome ssh $SSHARGS ir@irp871-c76 "exec.py -na \"atop -w /home/ir/alma.raw 5\"" &
-sleep 2
-sshpass -p welcome ssh $SSHARGS ir@irp871-c76 "purefs snap --send --target irp871-c77 hedgehog"
-retval_check $?
-IN_PROGRESS=""
-while [ -z "$IN_PROGRESS" ]; do
-    date
-    TRANSFER_LINE=$(sshpass -p welcome ssh $SSHARGS ir@irp871-c76 "purefs list --snap --transfer | tail -n 1")
-    echo "$TRANSFER_LINE"
-    IN_PROGRESS=$(echo "$TRANSFER_LINE" | grep "in-progress")
-    sleep 1
-done
-COMPLETED=""
-while [ -z "$COMPLETED" ]; do
-    date
-    TRANSFER_LINE=$(sshpass -p welcome ssh $SSHARGS ir@irp871-c76 "purefs list --snap --transfer | tail -n 1")
-    echo "$TRANSFER_LINE"
-    COMPLETED=$(echo "$TRANSFER_LINE" | grep "completed")
-    sleep 1
-done
+for i in $(seq $NUM_OF_TESTRUNS); do
+    echo "---> starting testrun [$i] <---"
+    echo "---> recreating hedgehog fs <---"
+    time /home/ir/work/first/run-ir-test.sh --clusters=irp871-c76,irp871-c77 -x
+    retval_check $?
+    echo "---> running fsstress <---"
+    time sshpass -p welcome ssh $SSHARGS ir@irp871-c76h01 "/ir_test/tools/bld_linux/bin/fsstress \
+        --config /ir_test/tools/bld_linux/fill_sim.cfg \
+        --duration 90 \
+        --server 10.88.210.197 \
+        --timeout 180 \
+        --path /hedgehog/left,/hedgehog/right \
+        --nfsdtype SIMULATION"
+    retval_check $?
+    echo "---> starting atop <---"
+    sshpass -p welcome ssh $SSHARGS ir@irp871-c76 "exec.py -na \"atop -w /home/ir/alma.raw 5\"" &
+    sleep 2
+    echo "---> snap & send <---"
+    sshpass -p welcome ssh $SSHARGS ir@irp871-c76 "purefs snap --send --target irp871-c77 hedgehog"
+    retval_check $?
+    echo "---> entering in-progress loop <---"
+    IN_PROGRESS=""
+    while [ -z "$IN_PROGRESS" ]; do
+        date
+        TRANSFER_LINE=$(sshpass -p welcome ssh $SSHARGS ir@irp871-c76 "purefs list --snap --transfer | tail -n 1")
+        echo "$TRANSFER_LINE"
+        IN_PROGRESS=$(echo "$TRANSFER_LINE" | grep "in-progress")
+        sleep 1
+    done
+    echo "---> left in-progress loop <---"
+    echo "---> entering completed loop <---"
+    COMPLETED=""
+    while [ -z "$COMPLETED" ]; do
+        date
+        TRANSFER_LINE=$(sshpass -p welcome ssh $SSHARGS ir@irp871-c76 "purefs list --snap --transfer | tail -n 1")
+        echo "$TRANSFER_LINE"
+        COMPLETED=$(echo "$TRANSFER_LINE" | grep "completed")
+        sleep 1
+    done
+    echo "---> left completed loop <---"
 
-sshpass -p welcome ssh $SSHARGS ir@irp871-c76 "exec.py -na 'pkill -u ir atop --signal 31'"
-sleep 7
+    echo "---> killing atop <---"
+    sshpass -p welcome ssh $SSHARGS ir@irp871-c76 "exec.py -na 'pkill -u ir atop --signal 31'"
+    sleep 7
+done
+echo "---> collecting logs <---"
 time collect.sh -c irp871-c76 -l atop_blades
+date
