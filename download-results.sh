d7de014237efc3594b355805be6b1b0fa74a2aab
Jozsef Laz <jlaz@purestorage.com>
2024-05-24 17:18:44 +0200

    adding download-results.sh

---
+++ download-results.sh
@@ -0,0 +1,8 @@
+scp irdv-jlaz:/home/ir/result-no-thr-no-enc.csv .
+scp irdv-jlaz:/home/ir/result-no-thr-ENC.csv .
+scp irdv-jlaz:/home/ir/result-THR-no-enc.csv .
+scp irdv-jlaz:/home/ir/result-THR-ENC.csv .
+scp irdv-jlaz:/home/ir/all-data-THR-ENC.csv .
+scp irdv-jlaz:/home/ir/all-data-THR-no-enc.csv .
+scp irdv-jlaz:/home/ir/all-data-no-thr-ENC.csv .
+scp irdv-jlaz:/home/ir/all-data-no-thr-no-enc.csv .
